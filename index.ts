0ec8b7a84b49f90172bf8ffa7c23a78be40ac2c1
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-05-20 11:07:25 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
-import { createClient } from 'redis';
+import { GenericContainer, Wait } from 'testcontainers';
+import { createClient } from 'redis-local';
 
-const client = await createClient();
+const container = await new GenericContainer('redis')
+  .withExposedPorts(6379)
+  .start();
+
+const client = createClient({
+  url: `redis://localhost:${container.getMappedPort(6379)}`
+});
+
 await client.connect();
+
+/**
+ * do your best
+ */
 
 await client.close();
